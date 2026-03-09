d41b944c7ae7105dc166e1dfaba45dd6bacef425
rohonDutta <rohonkumardutta@gmail.com>
2026-03-09 17:31:18 +0530

    Fix pip command in build_files.sh for Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
