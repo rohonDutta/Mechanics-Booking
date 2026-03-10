90fbebadfb9b9a991a2d65e7d0b67c2e83a49bf0
rohonDutta <rohonkumardutta@gmail.com>
2026-03-10 22:01:06 +0530

    commit

--- vehicleservicemanagement/settings.py
+++ vehicleservicemanagement/settings.py
@@ -66,7 +66,6 @@ WSGI_APPLICATION = 'vehicleservicemanagement.wsgi.application'
 # Database (Neon PostgreSQL)
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
         conn_max_age=600,
         ssl_require=True
     )
