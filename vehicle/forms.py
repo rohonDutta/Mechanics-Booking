eeb4a78360ab7e3b1c5b16e13017c304cd71a19c
rohonDutta <rohonkumardutta@gmail.com>
2026-03-11 21:30:43 +0530

    updated form

--- vehicle/forms.py
+++ vehicle/forms.py
@@ -1,43 +1,49 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 from . import models
+
+# --- REGEX VALIDATOR ---
+# Matches: 2 letters, space, 2 digits, space, 1 letter, space, 4 digits
+vehicle_pattern = RegexValidator(
+    regex=r'^[A-Za-z]{2}\s\d{2}\s[A-Za-z]{1}\s\d{4}$',
+    message='Enter vehicle number in format: "AB 12 C 1234"'
+)
 
 class CustomerUserForm(forms.ModelForm):
     class Meta:
-        model=User
-        fields=['first_name','last_name','username','password']
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'password']
         widgets = {
-        'password': forms.PasswordInput()
+            'password': forms.PasswordInput()
         }
 
 class CustomerForm(forms.ModelForm):
     class Meta:
-        model=models.Customer
-        fields=['address','mobile','profile_pic']
-
+        model = models.Customer
+        fields = ['address', 'mobile', 'profile_pic']
 
 class MechanicUserForm(forms.ModelForm):
     class Meta:
-        model=User
-        fields=['first_name','last_name','username','password']
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'password']
         widgets = {
-        'password': forms.PasswordInput()
+            'password': forms.PasswordInput()
         }
 
 class MechanicForm(forms.ModelForm):
     class Meta:
-        model=models.Mechanic
-        fields=['address','mobile','profile_pic','skill']
+        model = models.Mechanic
+        fields = ['address', 'mobile', 'profile_pic', 'skill']
 
 class MechanicSalaryForm(forms.Form):
-    salary=forms.IntegerField();
-
+    salary = forms.IntegerField()
 
 class RequestForm(forms.ModelForm):
-    # Explicitly define as CharField to allow letters and numbers
+    # Overriding vehicle_no to handle alphanumeric input and regex validation
     vehicle_no = forms.CharField(
-        label="Vehicle Number",
-        widget=forms.TextInput(attrs={'placeholder': 'Vehicle Number'})
+        validators=[vehicle_pattern],
+        widget=forms.TextInput(attrs={'placeholder': 'e.g. WB 12 C 1234', 'class': 'form-control'})
     )
 
     class Meta:
@@ -47,46 +53,45 @@ class RequestForm(forms.ModelForm):
             'problem_description': forms.Textarea(attrs={'rows': 3, 'cols': 30})
         }
 
+    # Automatically converts input to uppercase before saving
+    def clean_vehicle_no(self):
+        return self.cleaned_data['vehicle_no'].upper()
+
 class AdminRequestForm(forms.Form):
-    #to_field_name value will be stored when form is submitted.....__str__ method of customer model will be shown there in html
-    customer=forms.ModelChoiceField(queryset=models.Customer.objects.all(),empty_label="Customer Name",to_field_name='id')
-    mechanic=forms.ModelChoiceField(queryset=models.Mechanic.objects.all(),empty_label="Mechanic Name",to_field_name='id')
-    cost=forms.IntegerField()
+    customer = forms.ModelChoiceField(queryset=models.Customer.objects.all(), empty_label="Customer Name", to_field_name='id')
+    mechanic = forms.ModelChoiceField(queryset=models.Mechanic.objects.all(), empty_label="Mechanic Name", to_field_name='id')
+    cost = forms.IntegerField()
 
 class AdminApproveRequestForm(forms.Form):
-    mechanic=forms.ModelChoiceField(queryset=models.Mechanic.objects.all(),empty_label="Mechanic Name",to_field_name='id')
-    cost=forms.IntegerField()
-    stat=(('Pending','Pending'),('Approved','Approved'),('Released','Released'))
-    status=forms.ChoiceField( choices=stat)
-
+    mechanic = forms.ModelChoiceField(queryset=models.Mechanic.objects.all(), empty_label="Mechanic Name", to_field_name='id')
+    cost = forms.IntegerField()
+    stat = (('Pending', 'Pending'), ('Approved', 'Approved'), ('Released', 'Released'))
+    status = forms.ChoiceField(choices=stat)
 
 class UpdateCostForm(forms.Form):
-    cost=forms.IntegerField()
+    cost = forms.IntegerField()
 
 class MechanicUpdateStatusForm(forms.Form):
-    stat=(('Approved','Approved'),('Repairing','Repairing'),('Repairing Done','Repairing Done'))
-    status=forms.ChoiceField( choices=stat)
+    stat = (('Approved', 'Approved'), ('Repairing', 'Repairing'), ('Repairing Done', 'Repairing Done'))
+    status = forms.ChoiceField(choices=stat)
 
 class FeedbackForm(forms.ModelForm):
     class Meta:
-        model=models.Feedback
-        fields=['by','message']
+        model = models.Feedback
+        fields = ['by', 'message']
         widgets = {
-        'message':forms.Textarea(attrs={'rows': 6, 'cols': 30})
+            'message': forms.Textarea(attrs={'rows': 6, 'cols': 30})
         }
 
-#for Attendance related form
-presence_choices=(('Present','Present'),('Absent','Absent'))
+presence_choices = (('Present', 'Present'), ('Absent', 'Absent'))
 class AttendanceForm(forms.Form):
-    present_status=forms.ChoiceField( choices=presence_choices)
-    date=forms.DateField()
+    present_status = forms.ChoiceField(choices=presence_choices)
+    date = forms.DateField()
 
 class AskDateForm(forms.Form):
-    date=forms.DateField()
+    date = forms.DateField()
 
-
-#for contact us page
 class ContactusForm(forms.Form):
     Name = forms.CharField(max_length=30)
     Email = forms.EmailField()
-    Message = forms.CharField(max_length=500,widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
+    Message = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
